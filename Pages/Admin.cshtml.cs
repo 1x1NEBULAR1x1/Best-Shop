593856a928a7be37a189a68d28df9c8280cf543a
Kyrylo Belichenko <137775410+1x1NEBULAR1x1@users.noreply.github.com>
2025-02-12 21:58:43 +0100

    Update Admin.cshtml.cs

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace Best_Shop.Pages
 {
     public class AdminModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            if (userEmail != "admin@gmail.com")
+            {
+                return Forbid();
+            }
+
+            return Page();
         }
     }
 }
